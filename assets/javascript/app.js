cd5ebf54b491030e95ef0a455284870b74b1264b
Esar Behlum <esarnb@gmail.com>
2019-05-31 11:34:37 -0700

    Adding place holders for content and initializing list of q&a

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,82 @@
 //Global Variables
+var time = $("#time");
+var correction = $("#correction");
+var question = $("#question");
+var answers = $("#answers");
+
+//Questions
+
+var questions = {
+    q1: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q2: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q3: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q4: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q5: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q6: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q7: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q8: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q9: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    },
+    q10: {
+        prompt: "",
+        ans: [""],
+        correct: 0
+    }
+}
+
+
+/**
+ * PseudoCode
+ */
+//Setup Main Menu
+
+//Begin game using SetupFunction 
+
+    //Begin counter
+
+    //Show Q1 using QuestionFunction
+
+    //onClick, run show correct answer
+        //Highlight correct position of arrIndex
+
+    //Move to Q2 and repeat above and reset timer 
+
+
+    //After Q10 shown, show results. Pause.
+
+    //onClick playAgain, run SetupFunction
